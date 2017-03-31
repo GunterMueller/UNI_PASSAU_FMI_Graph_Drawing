25f15a968b855a6f2febb67bc956e70d3f363c8b
GunterMueller <gunter.mueller@gmail.com>
2017-03-30 21:55:13 -0400

    Modified graphed/error.h for Ubuntu

--- graphed-4.1.4/graphed/error.h
+++ graphed-4.1.4/graphed/error.h
@@ -7,7 +7,7 @@ extern	void	write_message (char *message);
 extern	void	message       (char *format, ...);
 extern	void	warning       (char *format, ...);
 extern	void	error         (char *format, ...);
-extern  void	sys_error     (int errno); 
+/*extern  void	sys_error     (int errno);  */
 extern	void	fatal_error   (char *format, ...);
 extern	void	die           (void);	/* main.c	*/
 extern	void	bypass_messages_to_file (FILE *file);
@@ -18,6 +18,7 @@ extern	void	bypass_messages_to_file (FILE *file);
 #include <errno.h>
 
 extern	int	sys_nerr;
-extern	char	*sys_errlist[];
+/* extern	char	*sys_errlist[]; */
+extern const char *const sys_errlist[];
 
 #endif
